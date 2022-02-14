d183cfa4e8a68655590368eb657d3013f37ab0d5
Greg Larden <93009716+GLARDEN@users.noreply.github.com>
2022-02-14 14:24:03 -0500

    Delete Audit directory

--- Audit/AuditType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestAuditTrail;
-
-public enum AuditType : byte
-{
-    None = 0,
-    Create = 1,
-    Update = 2,
-    Delete = 3
-}
--- Audit/IAuditable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestAuditTrail;
-public interface IAuditable { }
-
